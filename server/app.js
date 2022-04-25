6e833c920e1d38b62c804c5cea3496ef2ea095ea
isanaid <eizisana@gmail.com>
2022-04-25 22:07:18 +0700

    handle cors server

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app)
-const io = require('socket.io')(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:4000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
 
 io.on('connection', (socket) => {
     console.log('a user connected');
