d5c9c19e7834a16c169035155bc5c382af31699b
isanaid <eizisana@gmail.com>
2022-04-30 23:36:46 +0700

    add room and username before to room chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,53 @@
 import * as React from "react"
 import io from "socket.io-client"
 import "./App.css"
+import Chat from "./Chat";
 
 const socket = io.connect("http://localhost:4000");
 
 function App() {
-	const [room, setRoom] = React.useState("")
-	const [message, setMessage] = React.useState("")
-	const [messageReceived, setMessageReceived] = React.useState("")
+	const [username, setUsername] = React.useState("");
+	const [room, setRoom] = React.useState("");
+	const [showChat, setShowChat] = React.useState(false);
+	// const [message, setMessage] = React.useState("")
+	// const [messageReceived, setMessageReceived] = React.useState("")
 
 	const joinRoom = () => {
-		socket.emit("join_room", room)
+		if(username !== "" && room !== "") {
+		socket.emit("join_room", room);
+		setShowChat(true);
+		}
 	};
 
-	const sendMessage = () => {
-		socket.emit("send_message", { message, room });
-	};
+	// const sendMessage = () => {
+	// 	socket.emit("send_message", { message, room });
+	// };
 
-	React.useEffect(() => {
-		socket.on("receive_message", (data) => {
-			setMessageReceived(data.message);
-		});
-	}, []);
+	// React.useEffect(() => {
+	// 	socket.on("receive_message", (data) => {
+	// 		setMessageReceived(data.message);
+	// 	});
+	// }, []);
 
 	return (
 		<div className="App">
-			<h1>Chat App</h1>
+			{!showChat? (
+				<div>
+					<h1>Chat App</h1>
+					<input
+						type="text"
+						placeholder="Username"
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+					<input
+						type="text"
+						placeholder="Room"
+						onChange={(e) => setRoom(e.target.value)}
+					/>
+					<button onClick={joinRoom}>Join</button>
+				</div>
+					) : (<Chat socket={socket} username={username} room={room} />)};
+			{/* <h1>Chat App</h1>
 			<input
 				type="text"
 				placeholder="Room"
@@ -40,7 +62,7 @@ function App() {
 				onChange={(e) => setMessage(e.target.value)}
 			/>
 			<button onClick={sendMessage}>Send</button>
-			<p>{messageReceived}</p>
+			<p>{messageReceived}</p> */}
 		</div>
 	);
 }
